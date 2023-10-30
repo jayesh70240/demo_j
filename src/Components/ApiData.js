af44304ea9352d6460358b4e4bd30407f618c838
jayshree <jayeshhardapc@gmail.com>
2023-10-30 15:25:11 +0530

    pushing

---
+++ src/Components/ApiData.js
@@ -0,0 +1,28 @@
+// src/components/ApiData.js
+import React, { useState, useEffect } from 'react';
+
+function ApiData() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.example.com/data')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>API Data</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ApiData;
+
+
